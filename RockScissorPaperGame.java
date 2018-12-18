dd9a064f9cd6d3f2e083495e2c7b9f3f84f380b1
bayrambinbir <38406467+bayrambinbir@users.noreply.github.com>
2018-12-18 15:21:12 -0600

    Update RockScissorPaperGame.java

--- RockScissorPaperGame.java
+++ RockScissorPaperGame.java
@@ -6,14 +6,6 @@ import java.util.Scanner;
 public class RockScissorPaperGame {
 
 	public static void main(String[] args) {
-		/*
-		 * (Game: scissor, rock, paper) Write a program that plays the popular
-		 * scissor-rockpaper game. (A scissor can cut a paper, a rock can knock a
-		 * scissor, and a paper can wrap a rock.) The program randomly generates a
-		 * number 1, 2, and 3 representing scissor, rock, and paper. The program prompts
-		 * the user to enter a number 0, 1, or 2 and displays a message indicating
-		 * whether the user or the computer wins, loses, or draws.
-		 */
 		
 		System.out.println("SCISSOR, ROCK, PAPER");
 		System.out.println("Enter 1 for scissor OR 2 for rock OR 3 paper:");
